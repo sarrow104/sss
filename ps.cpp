6e7390dd8426df4a8385054122ac84d72477fb31
sarrow104 <sarrow104@gmail.com>
2016-10-07 11:12:00 +0800

    clang-format; fork() use dup2()

--- ps.cpp
+++ ps.cpp
@@ -1,39 +1,43 @@
 #include "ps.hpp"
 
 #include <sss/util/Escaper.hpp>
+#include <sss/util/PostionThrow.hpp>
 
 #ifdef __WIN32__
-#include <sss/process.hpp>
 #include <sss/environ.hpp>
+#include <sss/process.hpp>
 #else
-#include <sss/popenRWE.h>
+#include <sss/popenRWE.h>  // for pcloseRWE()
 
-namespace  {
+#include <fcntl.h>
+#include <unistd.h>
 
-int runPipe(int rwepipe[3],
-            const std::string& cmd,
-            const std::string& cwd,
+namespace {
+
+int runPipe(int rwepipe[3], const std::string& cmd, const std::string& cwd,
             const std::map<std::string, std::string>& env)
 {
-    const char * command = cmd.c_str();
+    const char* command = cmd.c_str();
     int in[2];
     int out[2];
     int err[2];
     int pid;
     int rc;
 
+    // NOTE 创建一个管道，并将读端，写端的描述符，分别赋值给in[0],in[1]
+    // 返回值 >= 0 表示 创建是否成功
     rc = ::pipe(in);
-    if (rc<0) {
+    if (rc < 0) {
         goto error_in;
     }
 
     rc = ::pipe(out);
-    if (rc<0) {
+    if (rc < 0) {
         goto error_out;
     }
 
     rc = ::pipe(err);
-    if (rc<0) {
+    if (rc < 0) {
         goto error_err;
     }
 
@@ -44,6 +48,15 @@ int runPipe(int rwepipe[3],
 
     pid = ::fork();
 
+    // NOTE 管道有一个特点，只能单向传输数据；即，fork()
+    // (描述符可以看做整数，并且父
+    // 子进程自动继承，并且可以正常访问)之后，必须根据传递方向，来手动关闭某一端；
+    // 比如，对于父到子的pipe，父关闭读端；而子则关闭写端；反之亦然。
+    //     0(Parent)     1(Child)
+    // --- --------- ------------
+    // in          w            r
+    // out         r            w
+    // err         r            w
     if (pid > 0) { /* parent */
         ::close(in[0]);
         ::close(out[1]);
@@ -57,28 +70,48 @@ int runPipe(int rwepipe[3],
         ::close(in[1]);
         ::close(out[0]);
         ::close(err[0]);
-        ::close(0);
-        if(!dup(in[0])) {
-            ;
-        }
-        ::close(1);
-        if(!dup(out[1])) {
-            ;
-        }
-        ::close(2);
-        if(!dup(err[1])) {
-            ;
-        }
 
-        for (std::map<std::string, std::string>::const_iterator it = env.begin();
-             it != env.end();
-             ++it)
-        {
+// NOTE
+// 关闭子进程本身的标准"输入输出错误"的文件描述符
+// 因为，输入输出错误，被重定向到管道中了
+#if 0
+        ::close(STDIN_FILENO);
+        if (!dup(in[0])) {
+            ;
+        }
+        ::close(STDOUT_FILENO);
+        if (!dup(out[1])) {
+            ;
+        }
+        ::close(STDERR_FILENO);
+        if (!dup(err[1])) {
+            ;
+        }
+#else
+        if (in[0] != STDIN_FILENO) {
+            dup2(in[0], STDIN_FILENO);
+            close(in[0]);
+        }
+        if (out[1] != STDOUT_FILENO) {
+            dup2(out[1], STDOUT_FILENO);
+            close(out[1]);
+        }
+        if (err[1] != STDERR_FILENO) {
+            dup2(err[1], STDERR_FILENO);
+            close(err[1]);
+        }
+#endif
+
+        for (std::map<std::string, std::string>::const_iterator it =
+                 env.begin();
+             it != env.end(); ++it) {
             ::setenv(it->first.c_str(), it->second.c_str(), 1);
         }
         (void)::chdir(cwd.c_str());
 
-        execl( "/bin/sh", "sh", "-c", command, NULL );
+        // NOTE 第二个参数，相当于 basename(arg1) (假设，参数序号从1开始）
+        // execl("/bin/sh", "sh", "-c", command, NULL);
+        execl("/bin/bash", "bash", "-c", command, NULL);
         _exit(1);
     }
     else {
@@ -103,62 +136,91 @@ error_in:
     return -1;
 }
 
-} // namespace
+}  // namespace
 #endif
 
-namespace sss
+namespace sss {
+namespace ps {
+fd_type RWEPipe::fork(const std::string& cmd, const std::string& wd,
+                      const std::map<std::string, std::string>& env)
 {
-    namespace ps
-    {
+    this->pid = ::runPipe(this->rwe, cmd, wd, env);
+    return this->pid;
+}
 
-        std::string PipeRun(const std::string& command_line,
-                            const std::string& dir,
-                            const std::map<std::string, std::string>& env)
-        {
+int RWEPipe::close() { return ::pcloseRWE(this->pid, this->rwe); }
+int RWEPipe::waitpid()
+{
+    int status;
+    return ::waitpid(pid, &status, 0);
+}
+std::string PipeRun(const std::string& command_line, const std::string& dir,
+                    const std::map<std::string, std::string>& env)
+{
 #ifdef __WIN32__
-            // TODO FIXME
-            std::string ret;
-            std::string env_str;
-            sss::env::dump(env_str, env);
-            sss::runPipeCmdLine(command_line, dir, env_str, ret);
+    // TODO FIXME
+    std::string ret;
+    std::string env_str;
+    sss::env::dump(env_str, env);
+    sss::runPipeCmdLine(command_line, dir, env_str, ret);
 
-            return ret;
+    return ret;
 #else
-            std::ostringstream oss;
-            int rwe_pipe[3];
-            int pid = ::runPipe(rwe_pipe, command_line, dir, env);
-            char buf[1024];
-            while(true) {
-                size_t cnt = ::read(rwe_pipe[1], buf, sizeof(buf));
-                if (!cnt) {
-                    break;
-                }
-                oss.write(buf, cnt);
-            }
-            int status;
-            int rc = ::waitpid(pid, &status, 0);
-            if (rc == -1) {
-                return "";
-            }
+    std::ostringstream oss;
+    int rwe_pipe[3];
+    int pid = ::runPipe(rwe_pipe, command_line, dir, env);
 
-            pcloseRWE(pid, rwe_pipe);
-            return oss.str();
+// NOTE 文件描述符的阻塞与非阻塞
+#ifdef SSS_NONBLOCKING_FILEDESCRIPTOR
+    // set non-block of in_fd
+    int fl = ::fcntl(rwe_pipe[1], F_GETFL, 0);
+    ::fcntl(rwe_pipe[1], F_SETFL, fl | O_NONBLOCK);
 #endif
-        }
 
-        // system 在linux和windows下，都是调用自己的命令行解释器，对目录进行解释；
-        // linux下，自然就是shell；
-        // shell下，对参数的特殊字符，有两种处理办法；一种是用引号（单双皆可）
-        // ，来消除歧义；一种，就是用反斜杠进行转义；
-        //
-        // 不过，有几个字符比较特殊，应按照需要来转义；
-        // '>','<','|',还有"&&"，"||"
-        // 前三个是重新定向管道；后面两个是短路执行；
-        // 绝大多数情况下（很少有人的文件名，直接是'>',...；），都OK；但来自网
-        // 络的文件名，确实可能含有'&'字符；
-        // 即，真有可能需要区分情况，以决定是否用引号包裹，或者转义；
-        void StringPipe::streamAddParam(std::ostream& oss, const std::string& param)
-        {
+    char buf[1024];
+    while (true) {
+        ssize_t cnt = ::read(rwe_pipe[1], buf, sizeof(buf));
+        if (cnt < 0) {
+            // NOTE 只有当fd被设置为 O_NONBLOCK 的时候，errno才会因为该
+            // fd，未准备好，而将errno 设置为EAGAIN == try again
+            if (errno != EAGAIN) {
+                SSS_POSTION_THROW(std::runtime_error, strerror(errno));
+            }
+        }
+        if (cnt <= 0) {
+            break;
+        }
+        oss.write(buf, cnt);
+    }
+
+#ifdef SSS_NONBLOCKING_FILEDESCRIPTOR
+    ::fcntl(rwe_pipe[1], F_SETFL, fl);
+#endif
+
+    int status;
+    int rc = ::waitpid(pid, &status, 0);
+    if (rc == -1) {
+        return "";
+    }
+
+    pcloseRWE(pid, rwe_pipe);
+    return oss.str();
+#endif
+}
+
+// system 在linux和windows下，都是调用自己的命令行解释器，对目录进行解释；
+// linux下，自然就是shell；
+// shell下，对参数的特殊字符，有两种处理办法；一种是用引号（单双皆可）
+// ，来消除歧义；一种，就是用反斜杠进行转义；
+//
+// 不过，有几个字符比较特殊，应按照需要来转义；
+// '>','<','|',还有"&&"，"||"
+// 前三个是重新定向管道；后面两个是短路执行；
+// 绝大多数情况下（很少有人的文件名，直接是'>',...；），都OK；但来自网
+// 络的文件名，确实可能含有'&'字符；
+// 即，真有可能需要区分情况，以决定是否用引号包裹，或者转义；
+void StringPipe::streamAddParam(std::ostream& oss, const std::string& param)
+{
 #if 0
             if (param.empty()) {
                 return;
@@ -183,21 +245,21 @@ namespace sss
                 oss << '"';
             }
 #else
-            // linux:
-            // static sss::util::Escaper esp(R"(\ "'[](){}?*$)"); // -std=c++11
-            // windows-cmd需要引号的特殊字符是:
-            //     <space>
-            //     &()[]{}^=;!'+,`~
-            static sss::util::Escaper esp("\\ \"'[](){}?*$&");
-            esp.escapeToStream(oss, param);
+    // linux:
+    // static sss::util::Escaper esp(R"(\ "'[](){}?*$)"); // -std=c++11
+    // windows-cmd需要引号的特殊字符是:
+    //     <space>
+    //     &()[]{}^=;!'+,`~
+    static sss::util::Escaper esp("\\ \"'[](){}?*$&");
+    esp.escapeToStream(oss, param);
 #endif
-        }
-        void StringPipe::streamAddParams(std::ostream& o, int argc, char *argv[])
-        {
-            for (int i = 0; i < argc; ++i) {
-                o << " ";
-                StringPipe::streamAddParam(o, argv[i]);
-            }
-        }
+}
+void StringPipe::streamAddParams(std::ostream& o, int argc, char* argv[])
+{
+    for (int i = 0; i < argc; ++i) {
+        o << " ";
+        StringPipe::streamAddParam(o, argv[i]);
     }
+}
+}
 }
