e19290487e505cd6c1941d2937818adfd92bb8b3
sarrow104 <sarrow104@gmail.com>
2020-09-08 14:34:15 +0800

    macro: cast to string

---
+++ macro/cast_to_string.hpp
@@ -0,0 +1,10 @@
+// macro/cast_to_string.hpp
+#ifndef __CAST_TO_STRING_HPP_1569564126__
+#define __CAST_TO_STRING_HPP_1569564126__
+
+#include <boost/preprocessor/cat.hpp>
+
+#define SSS_PP_CAST_TO_STRING( __X__ ) #__X__
+
+
+#endif /* __CAST_TO_STRING_HPP_1569564126__ */
