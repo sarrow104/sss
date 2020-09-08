c99c8b9a0d400e12306090603226120eed360799
sarrow104 <sarrow104@gmail.com>
2020-09-08 14:29:14 +0800

    add wait_anykey() for debug help

---
+++ debug/wait_anykey.hpp
@@ -0,0 +1,10 @@
+// http://www.cplusplus.com/forum/articles/7312/
+
+#include <string>
+
+namespace sss {
+namespace debug {
+int wait_anykey( const std::string& prompt = "Press a key to continue..." );
+} // namespace debug
+} // namespace sss
+
