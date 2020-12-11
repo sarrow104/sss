e75266780cbdbaf88df9eda7ce8a9489dce4b87c
sarrow104 <sarrow104@gmail.com>
2020-12-11 18:19:48 +0800

    code clean

--- config.hpp
+++ config.hpp
@@ -1,7 +1,10 @@
+// config.hpp
 #ifndef __CONFIG_HPP_1556974397__
 #define __CONFIG_HPP_1556974397__
 
-#if __cplusplus >= 201103L
+#include <sss/macro/cppstandard.hpp>
+
+#if SSS_STD_GE_11()
 #define SSS_NOEXCEPT noexcept
 #else
 #define SSS_NOEXCEPT throw()
@@ -9,7 +12,7 @@
 
 // __cplusplus > 201402L
 
-#if __cplusplus >= 201703L
+#if SSS_STD_EQ_17()
 #define SSS_FALLTHROUGH [[fallthrough]]
 #else
 #define SSS_FALLTHROUGH __attribute__((fallthrough))
